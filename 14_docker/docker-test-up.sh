b7e3f2649ed76d4296595994701c2b18b00bc646
LesleyMacconaughey <dm.ryzhov@yandex.ru>
2025-01-27 04:26:02 +0300

    hw14

--- 14_docker/docker-test-up.sh
+++ 14_docker/docker-test-up.sh
@@ -12,4 +12,4 @@ yc compute instance create \
   --ssh-key ~/.ssh/id_rsa.pub
 EXTERNAL_IP=$(yc compute instance get $VM_NAME --format json | jq -r '.network_interfaces[].primary_v4_address.one_to_one_nat.address')
 echo "VM external IP: $EXTERNAL_IP"
-yq -i ".webservers.hosts.hw-rpm.ansible_host = \"$EXTERNAL_IP\"" rpm-create/inventory/hosts
+yq -i ".webservers.hosts.hw-rpm.ansible_host = \"$EXTERNAL_IP\"" docker-hw/inventory/hosts

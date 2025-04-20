d6936349af1c342bbb7f3947568b4a93d5ef4269
LesleyMacconaughey <dm.ryzhov@yandex.ru>
2025-04-20 13:46:50 +0300

    hw18

--- 18_backup/down-vm.sh
+++ 18_backup/down-vm.sh
@@ -1,7 +1,38 @@
-yc compute instance delete web
-yc compute instance delete log
+#!/bin/bash
+
+# Удаление ВМ
+yc compute instance delete backup-server
+yc compute instance delete client
+
+# Отвязка таблицы маршрутизации от подсети
 yc vpc subnet update \
   --name default-ru-central1-b \
   --route-table-name ""
-yc vpc route-table delete nat-route
-yc vpc gateway delete nat-gateway
+
+# Удаление таблиц маршрутизации
+route_table_ids=$(yc vpc route-table list --format json | jq -r '.[] | select(.name == "nat-route") | .id')
+
+if [ -z "$route_table_ids" ]; then
+    echo "Таблиц маршрутизации с именем 'nat-route' не найдено."
+else
+    # Удаление каждой найденной таблицы
+    for id in $route_table_ids; do
+        echo "Удаление таблицы маршрутизации с ID: $id"
+        yc vpc route-table delete $id
+    done
+fi
+
+# Удаление NAT-шлюзов
+gateway_ids=$(yc vpc gateway list --format json | jq -r '.[] | select(.name == "nat-gateway") | .id')
+
+if [ -z "$gateway_ids" ]; then
+    echo "Шлюзов с именем 'nat-gateway' не найдено."
+else
+    # Удаление каждого найденного шлюза
+    for id in $gateway_ids; do
+        echo "Удаление шлюза с ID: $id"
+        yc vpc gateway delete $id
+    done
+fi
+
+echo "Все компоненты инфраструктуры успешно удалены."
--- 18_backup/up-vm.sh
+++ 18_backup/up-vm.sh
@@ -14,23 +14,56 @@ yc vpc subnet update \
   --name default-ru-central1-b \
   --route-table-name nat-route
 
-# Создаем виртуальнуюмашину web
-VM_NAME="web"
+# Создаем виртуальнуюмашину backup_server
+VM_NAME="backup-server"
 yc compute instance create \
   --name $VM_NAME \
   --hostname $VM_NAME \
   --zone ru-central1-b \
   --network-interface subnet-name=default-ru-central1-b,nat-ip-version=ipv4 \
   --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-2204-lts \
+  --create-disk size=2G,type=network-ssd,auto-delete=true \
   --memory 2G \
   --cores 2 \
   --core-fraction 5 \
   --preemptible \
-  --ssh-key ~/.ssh/id_rsa.pub
+  --metadata-from-file user-data=<(cat <<EOF
+#cloud-config
+users:
+  - name: yc-user
+    ssh-authorized-keys:
+      - $(cat ~/.ssh/id_rsa.pub)
+    groups: sudo
+    shell: /bin/bash
 
+packages:
+  - borgbackup
+
+runcmd:
+  # Настройка sudo без пароля для пользователя yc-user
+  - echo "yc-user ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/yc-user-nopasswd
+  - chmod 440 /etc/sudoers.d/yc-user-nopasswd
+  
+  # Настройка диска для бэкапов
+  - mkfs.ext4 /dev/vdb
+  - mkdir -p /var/backup
+  - mount /dev/vdb /var/backup
+  - echo "/dev/vdb /var/backup ext4 defaults 0 0" >> /etc/fstab
+  
+  # Создание пользователя для бэкапов
+  - useradd -m -s /bin/bash borg
+  - mkdir -p /home/borg/.ssh
+  - touch /home/borg/.ssh/authorized_keys
+  - chown -R borg:borg /home/borg/.ssh
+  - chmod 700 /home/borg/.ssh
+  - chmod 600 /home/borg/.ssh/authorized_keys
+  - chown borg:borg /var/backup
+EOF
+)
 EXTERNAL_IP=$(yc compute instance get $VM_NAME --format json | jq -r '.network_interfaces[].primary_v4_address.one_to_one_nat.address')
 
-VM_NAME="log"
+# Создаем виртуальнуюмашину client
+VM_NAME="client"
 yc compute instance create \
   --name $VM_NAME \
   --hostname $VM_NAME \
@@ -41,18 +74,27 @@ yc compute instance create \
   --cores 2 \
   --core-fraction 5 \
   --preemptible \
-  --ssh-key ~/.ssh/id_rsa.pub
+  --metadata-from-file user-data=<(cat <<EOF
+#cloud-config
+users:
+  - name: yc-user
+    ssh-authorized-keys:
+      - $(cat ~/.ssh/id_rsa.pub)
+    groups: sudo
+    shell: /bin/bash
 
-# Внесение изменений в инвентори
-yq -i ".all.hosts.webserver.ansible_host = \"$EXTERNAL_IP\"" hw/inventory/hosts
-yq -i '.all.hosts.logserver.ansible_ssh_common_args = "-o ProxyJump=yc-user@'"$EXTERNAL_IP"'"' hw/inventory/hosts
-echo "Ожидание создания виртуальных машин..."
-sleep 60
-echo "Запуск ansible-playbook..."
-ansible-playbook -i hw/inventory/hosts hw/playbook.yml
-echo "Ansible-playbook завершен."
-echo "Создано 2 виртуальные машины web и log в Yandex Cloud."
-echo "Для удаленного доступа к виртуальной машине web используйте команду:"
+packages:
+  - borgbackup
+
+runcmd:
+  # Настройка sudo без пароля для пользователя yc-user
+  - echo "yc-user ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/yc-user-nopasswd
+  - chmod 440 /etc/sudoers.d/yc-user-nopasswd
+    
+EOF
+)
+
+echo "Для удаленного доступа к виртуальной машине backup-server используйте команду:"
 echo "ssh yc-user@$EXTERNAL_IP"
-echo "Для удаленного доступа к виртуальной машине log используйте команду:"
-echo "ssh -J yc-user@$EXTERNAL_IP yc-user@log"
+echo "Для удаленного доступа к виртуальной машине client используйте команду:"
+echo "ssh -J yc-user@$EXTERNAL_IP yc-user@client"
